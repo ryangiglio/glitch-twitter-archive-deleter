71c0ab5fae9a45a70036de8e7d1ea8563169cb07
Ryan Giglio <ryangiglio@gmail.com>
2018-10-30 21:22:21 -0400

    Load tweet archive in the root

--- twitter-deleter/loadTweetArchive.js
+++ twitter-deleter/loadTweetArchive.js
@@ -4,10 +4,10 @@ const csv = require('fast-csv')
 module.exports = async () => {
   // Return a promise for when the whole processing is finished
   return new Promise(async (funcResolve, funcReject) => {
-    const stream = fs.createReadStream('./data/tweets.csv')
+    const stream = fs.createReadStream('./tweets.csv')
 
     stream.on('error', err => {
-      funcReject(new Error('Could not load file `data/tweets.csv`'))
+      funcReject(new Error('Could not load file `./tweets.csv`'))
     })
 
     // Use a promise to wait for processing of the CSV to complete
