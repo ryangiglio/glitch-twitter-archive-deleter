9a1b48a4cc37604019a7450841ed8d7a87c2b160
Ryan Giglio <ryangiglio@gmail.com>
2018-11-03 17:31:07 -0400

    Rename the envars

--- server/twitter-deleter/twitterClient.js
+++ server/twitter-deleter/twitterClient.js
@@ -4,8 +4,8 @@ require('dotenv').config()
 const Twitter = require('twitter')
 
 const twitterClient = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  consumer_key: process.env.TWITTER_CONSUMER_API_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_API_SECRET_KEY,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 })
